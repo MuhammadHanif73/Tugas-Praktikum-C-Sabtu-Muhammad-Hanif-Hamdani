c69e7231cdf39fc0ed0508f5c8bb3af801424ac5
MuhammadHanif73 <m.hanif.hamdani@gmail.com>
2024-11-01 16:59:59 +0700

    second commit

--- Tugas 2/1. Luas segitiga.cpp
+++ Tugas 2/1. Luas segitiga.cpp
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main()
-{	
-	
+{		
 	float alas = 8.0;
 	float tinggi = 5.0;
 	float luas = 0.5 * alas * tinggi;
@@ -12,6 +11,6 @@ int main()
 	printf("Tinggi: %2.f cm\n", tinggi);
 	printf("Rumus Luas segitiga: 1/2 x alas x tinggi\n");
 	printf("hasil Luasnya: %2.f cm^2", luas);
-	
+
 	return 0;
 }
